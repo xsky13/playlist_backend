8fbc4e0c87d79f1b5874ed68197aeebcb37a34a5
Jared Peter <aljapeter05@gmail.com>
2026-06-30 23:27:56 -0300

    added origin

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://xsky13.github.io',
     optionsSuccessStatus: 200
 }));
 

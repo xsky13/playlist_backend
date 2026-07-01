6bb572d20d78252029db23f6cc0436fabdca9618
Jared Peter <aljapeter05@gmail.com>
2026-06-30 23:23:28 -0300

    minor fixes

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ app.use(cors({
 
 function parseISO8601Duration(duration) {
     const match = duration.match(/PT(?:(\d+)H)?(?:(\d+)M)?(?:(\d+)S)?/);
+        if (!match) return null;
     const hours = parseInt(match[1] || "0", 10);
     const minutes = parseInt(match[2] || "0", 10);
     const seconds = parseInt(match[3] || "0", 10);
